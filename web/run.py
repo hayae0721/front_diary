9e3e5c231882dc7568c4e767bac272976a57473f
KYUSEONGHAN <tyty587587@gmail.com>
2022-11-06 21:06:56 +0900

    2022.11.6.일: hks, 일기 텍스트 부분 전처리 기능 개발, python3

--- web/run.py
+++ web/run.py
@@ -6,4 +6,6 @@ def hello():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(port="9999", debug=True)
+    # app.run(host="0.0.0.0", port="9999", debug=True)
+    app.run(host="0.0.0.0", port="9999", debug=False)
+    # app.run(host='192.168.0.58', port='9000', debug=False)
